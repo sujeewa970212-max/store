363cd0d8cfe23b364335b2efeb774f5ef1225788
sujeewa970212-max <sujeewa970212@gmail.com>
2026-08-31 22:22:26 +0530

    Update product-details.js

--- js/product-details.js
+++ js/product-details.js
@@ -20,25 +20,25 @@ rating:"★★★★★",
 /*stock:"In Stock",*/
 
 description:
-"Product Model	RichAuto----A11
-CPU	DSP
-Internal Memory	512Mb
-Display Screen	128*64 Monochrome LCD
-Communication Port	U disk、USB cable
-Controlled Axis Number	3 axis
-Control Signal	differential signal
-Drive System	step motor/servo motor
-Minimum Input Unit	0.001mm
-No Volt Protection	Support
-Breakpoint Processing Function	Support
-External Power Supply Voltage	24V DC
-Manual Mode	Continuous、point move、distance
-Interpolation Method	line、arc、curve
-Soft/Hard Limit	support
-Maximum Pulse Frequency	1M/S
-Password Protection	support
-Support Language	simple Chinese/traditional Chinese、English、other languages
-Standard Configuration	(1) DSP Handle；(1) 50 core cable (1)USB cable； (1)8 I/O Wiring board;(1) data CD"
+    "Product Model	RichAuto----A11"
+    "CPU	DSP"
+    "Internal Memory	512Mb"
+    "Display Screen	128*64 Monochrome LCD"
+    "Communication Port	U disk、USB cable"
+    "Controlled Axis Number	3 axis"
+    "Control Signal	differential signal"
+    "Drive System	step motor/servo motor"
+    "Minimum Input Unit	0.001mm"
+    "No Volt Protection	Support"
+    "Breakpoint Processing Function	Support"
+    "External Power Supply Voltage	24V DC"
+    "Manual Mode	Continuous、point move、distance"
+    "Interpolation Method	line、arc、curve"
+    "Soft/Hard Limit	support"
+    "Maximum Pulse Frequency	1M/S"
+    "Password Protection	support"
+    "Support Language	simple Chinese/traditional Chinese、English、other languages"
+    "Standard Configuration	(1) DSP Handle；(1) 50 core cable (1)USB cable； (1)8 I/O Wiring board;(1) data CD"
 
 },
 
